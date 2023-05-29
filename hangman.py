46c413755ad4e19f95e11d0fe123fdc8523ee40e
sreejesh333 <sreejeshravindran523@gmail.com>
2023-05-29 15:40:54 +0530

    passes nine functions
    test_get_word(),test_masked_word_correctguesses(),test_masked_word_wrongguesses(),test_masked_word_noguess(),test_masked_word_repeatedletters(),test_select_random_word_min_length(), test_select_random_word_no_non_alpha_chars() , test_select_random_word_no_capitals(),test_select_random_word_no_repetitions()

---
+++ hangman.py
@@ -0,0 +1,122 @@
+import random
+
+def get_random_word(wordfile = "/usr/share/dict/words"):
+    candidate_words = []
+    with open(wordfile) as f: 
+        for word in f:
+            word = word.strip()
+            if len(word) >= 6 and word.islower() and word.isalpha() :
+                candidate_words.append(word)
+    word = random.choice(candidate_words)
+    return word
+
+
+def get_word(word) :
+    len_wrds = "_" * len(word)
+
+    return len_wrds
+
+def display_tries(tries):
+    
+    tries -= 1
+    return tries
+
+
+def usr_input():
+    return input("enter the letter :").lower()
+
+def masked_word(word , guess) :
+    guess_wrd = []
+    for i in word :
+            if i in guess :
+                guess_wrd.append(i) 
+            # elif i not in guess :
+            #     guess_wrd.append("_")   
+            else :
+                guess_wrd.append("_")
+    
+    return "".join(guess_wrd)
+            
+
+
+
+        
+
+
+
+
+
+
+# def play_hang(word) :
+#     wrd_completion = get_word(word)
+#     guessed = False
+#     guess_letter = []
+#     tried = 6
+#     print(wrd_completion)
+    
+#     while not guessed and tried > 0 :
+#         guess = usr_input()
+
+        
+#         if len(guess) == 1 and guess.isalpha() :
+#             if guess  in guess_letter :
+#                 print("already written letter")
+            
+#             elif guess not in word :
+#                 tried = display_tries(tried)
+#                 print("no of tries left :",tried)
+#                 print(wrd_completion)
+#                 guess_letter.append(guess)
+            
+#             else :
+#                 guess_letter.append(guess)
+#                 wrd_list = list(wrd_completion)
+#                 indexnum = []
+#                 for i , letter in enumerate(word) :
+#                     if letter == guess :
+#                         indexnum.append(i)
+#                 for index in indexnum :
+#                     wrd_list[index*2] = guess
+#                     wrd_completion = "".join(wrd_list)
+#                     if "_" not in wrd_completion :
+#                         guessed = True
+#                         # print(guessed)
+#                 print(wrd_completion)
+                
+        
+
+#         else :
+#             print("wrong guess")
+#             print(wrd_completion)
+#             print("\n")
+
+
+#     if guessed == True :
+#         print("u guessed right ...the word is :"+word)
+
+        
+#     else :
+#         print("sorry u loose , the word is :"+ word )
+
+       
+    
+   
+   
+
+def main():
+    word = get_random_word()
+    play_hang(word)
+    
+    while input("do u want a continue (y/n):") == 'y' :
+        word =  get_random_word()
+        play_hang(word)
+
+    
+if __name__ == "__main__":
+    main()
+
+
+
+
+                        
+                 
---
+++ test_hangman.py
@@ -0,0 +1,83 @@
+import os
+import tempfile
+import hangman
+
+def test_select_random_word_min_length():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["cat\n","elephant\n","mouse\n","dog\n",])
+    
+    f.close()
+
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "elephant"
+
+    os.unlink(name)
+
+def test_select_random_word_no_non_alpha_chars():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["pine's\n","Dr.\n","Ångström\n","elephant\n"])
+    f.close()
+
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "elephant"
+
+    os.unlink(name)
+
+def test_select_random_word_no_capitals():
+    # create temporary file
+    name = tempfile.mktemp()
+    f = open(name, "w")
+    f.writelines(["Alexander\n","AMD\n","California\n","elephant\n"])
+    f.close()
+
+    for _ in range(20):
+        secret_word = hangman.get_random_word(name)
+        assert secret_word == "elephant"
+
+    os.unlink(name)
+
+
+def test_select_random_word_no_repetitions():
+    secret_words = set()
+    for _ in range(10):
+        secret_words.add(hangman.get_random_word())
+    assert len(secret_words) == 10
+    
+def test_get_word():
+    secret_word = hangman.get_word("apartment")
+    assert secret_word == "_________ "
+
+# def test_display_tries():
+#     try_check = hangman.display_tries(6)
+#     assert try_check == 5
+
+def test_masked_word_correctguesses() :
+    assert hangman.masked_word("elephant" , ["p"]) == "___p____"
+
+def test_masked_word_wrongguesses() :
+    assert hangman.masked_word("elephant" , ["x"]) == "________"
+
+def test_masked_word_noguess() :
+    assert hangman.masked_word("elephant" , [" "]) == "________"
+
+def test_masked_word_repeatedletters() :
+    assert hangman.masked_word("elephant" , ["e"]) == "e_e_____"
+
+
+
+    
+
+
+
+
+
+
+
+
+    
